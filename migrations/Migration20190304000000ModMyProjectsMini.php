4da9a57979eaeae352e4b406ffd3164df9db002d
M. Drew LaMar <mdlama@wm.edu>
2021-02-17 11:07:35 -0500

    update copyright/trademark ownership
    (cherry picked from commit 7a772265691cebdda2695389145abe67bd7bcd96)

--- migrations/Migration20190304000000ModMyProjectsMini.php
+++ migrations/Migration20190304000000ModMyProjectsMini.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
 
 use Hubzero\Content\Migration\Base;
 
